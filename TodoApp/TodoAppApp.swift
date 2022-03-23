df600be73d1e9dd897ef6c5fc0b337b70da4dc6d
magisystem0408 <masato.matsudo@gmail.com>
2022-03-23 11:09:51 +0900

    Initial Commit

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by 松戸誠人 on 2022/03/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
